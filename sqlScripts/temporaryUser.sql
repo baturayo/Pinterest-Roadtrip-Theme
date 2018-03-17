aee65f4a1cf6305cafcfcf59a1e7a1635650c5bf
cekefun <cekefun@gmail.com>
2018-03-17 13:54:38 +0100

    Some sql scripts Cedric uses to test

---
+++ sqlScripts/temporaryUser.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  cekef
+ * Created: Mar 17, 2018
+ */
+
+delete from loggedintimestamps where user_id = 1;
+delete from user where id = 1;
+insert into user values (1,"a","a","a",1,"a","a","a");
---
+++ sqlScripts/timestamps.sql
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  cekef
+ * Created: Mar 17, 2018
+ */
+delete from loggedintimestamps where true;
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-12 17");
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-13 17");
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-14 17");
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-15 17");
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-15 16");
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-16 15");
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-16 16");
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-16 17");
+insert into loggedintimestamps (user_id,moment) values(1,"2018-03-16 18");
