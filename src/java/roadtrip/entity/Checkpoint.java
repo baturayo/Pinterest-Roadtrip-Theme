55ddd9fafb5b79c6147bc695a8ffe3ec0b4326f3
Michael Adriaensen <michaeladriaensen@gmail.com>
2018-04-04 14:09:54 +0200

    added entity classes checkpoint and photo and facades. fixed link that still pointed to /login.

---
+++ src/java/roadtrip/entity/Checkpoint.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roadtrip.entity;
+
+import java.awt.geom.Point2D;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Michael
+ */
+@Entity
+public class Checkpoint implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private Integer id;
+    
+    @Basic(optional = false)
+    @Column(name="name")
+    @NotNull
+    private String name;
+    
+    @Basic(optional = false)
+    @Column(name="description")
+    @NotNull
+    private String description;
+    
+    
+    @Basic(optional = false)
+    @Column(name="coordinates")
+    @NotNull
+    private Point2D.Double coordinates;
+    
+    @Basic(optional = false)
+    @Column(name="countryid")
+    @NotNull
+    private Integer countryid;
+
+    public Integer getCountryid() {
+        return countryid;
+    }
+
+    public void setCountryid(Integer countryid) {
+        this.countryid = countryid;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Point2D.Double getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Point2D.Double coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Checkpoint)) {
+            return false;
+        }
+        Checkpoint other = (Checkpoint) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "roadtrip.entity.Checkpoint[ id=" + id + " ]";
+    }
+    
+}
